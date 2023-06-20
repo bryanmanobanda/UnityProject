579e940a6a7f5083a84559f24fee1b90175cccc7
KelvinOjedaEpn <kelvin.ojeda@epn.edu.ec>
2023-06-20 00:25:39 -0500

    Add: Movement of legs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,10 +9,12 @@ public class Movement : MonoBehaviour
     public float jumpspeed = 4f;
 
     Rigidbody2D body;
+    Animator animations;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animations = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -21,19 +23,23 @@ public class Movement : MonoBehaviour
         {
             body.velocity = new Vector2(-speed, body.velocity.y);
             GetComponent<SpriteRenderer>().flipX = true;
+            animations.SetBool("Run", true);
         }
         else if (Input.GetKey("d") || Input.GetKey("right"))
         {
             body.velocity = new Vector2(speed, body.velocity.y);
             GetComponent < SpriteRenderer > ().flipX = false;
+            animations.SetBool("Run", true);
         }
         else
         {
             body.velocity = new Vector2 (0, body.velocity.y);
+            animations.SetBool("Run", false);
         }
         if(CheckGrounded.isGrounded && Input.GetKey("space"))
         {
             body.velocity = new Vector2(body.velocity.x, jumpspeed);
+            
         }
     }
 
